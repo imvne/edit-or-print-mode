c5b3b336b597ba2c01ba895e0521f60c96770289
imvne <imanebeaufils19@gmail.com>
2024-09-11 21:19:38 +0200

    finit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Greeting from '../greeting.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
